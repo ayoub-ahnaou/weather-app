9374c02749e1d25fb039c0704174800cb239f0ad
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-09-21 13:08:58 +0100

    javascript finished

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const apiKey = "add930fe9e8b800d621b44b3233e6253";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=france";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric";
+
+const image = document.getElementById("image");
+const temperature = document.getElementById("temperature");
+const city = document.getElementById("city");
+const humidity = document.getElementById("humidity");
+const wind = document.getElementById("wind");
+
+const city_input = document.getElementById("city_input");
+const btn = document.getElementById("btn");
 
 async function checkWeather() {
-    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+    const response = await fetch(apiUrl + `&q=${city_input.value}` + `&appid=${apiKey}`);
     let data = await response.json();
 
-    console.log(data);
+    image.src = "images/" + data.weather[0].main + ".png";
+    temperature.innerHTML = Math.round(data.main.temp) + "° C";
+    city.innerHTML = data.name;
+    humidity.innerHTML = data.main.humidity + " %";
+    wind.innerHTML = data.wind.speed + " km/h";
+
 }
 
-checkWeather();
+btn.addEventListener("click", () => {
+    checkWeather();
+});
 
